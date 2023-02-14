45526c1c2094c838549bd81d39ea4586581ab413
TUNG N KIM <Tungkims@gmail.com>
2023-02-14 16:09:12 -0600

    Made a Create Game View

---
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
