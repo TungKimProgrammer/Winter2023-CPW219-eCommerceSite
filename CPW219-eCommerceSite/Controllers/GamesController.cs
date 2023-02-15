39032e71d4eff0c6464444efcc1ec7b2e9c501c8
TUNG N KIM <Tungkims@gmail.com>
2023-02-14 18:59:58 -0600

    Create Game fully functional

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -1,14 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+        // DO NOT REASSIGN THIS 
+        private readonly GameContext _context;
+
+        public GamesController(GameContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game g)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add to DB
+                _context.Games.Add(g); // Prepares insert
+                _context.SaveChanges(); // Executes pending statements
+
+                // Show success message on page
+                ViewData["Message"] = $"{g.Title} was added successfully!";
+                return View();
+            }
+
+            return View(g);
         }
     }
 }
