9cdbeaadc11561aac8b73de9abe7d807dbaea753
TUNG N KIM <Tungkims@gmail.com>
2023-02-15 00:46:42 -0600

    Added Edit view and hid the GameId from the users

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -49,5 +49,17 @@ namespace CPW219_eCommerceSite.Controllers
 
             return View(g);
         }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Game? gameToEdit = await _context.Games.FindAsync(id);
+
+            if (gameToEdit == null)
+            {
+                return NotFound();
+            } 
+
+            return View(gameToEdit);
+        }
     }
 }
