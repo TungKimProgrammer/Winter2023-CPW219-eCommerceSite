3d1cc215b9ae81cd2b37135a592778527b43eadc
TUNG N KIM <Tungkims@gmail.com>
2023-02-25 01:31:30 -0600

    Log out functionality works

--- CPW219-eCommerceSite/Controllers/MembersController.cs
+++ CPW219-eCommerceSite/Controllers/MembersController.cs
@@ -36,6 +36,9 @@ namespace CPW219_eCommerceSite.Controllers
                 _context.Members.Add(newMember);
                 await _context.SaveChangesAsync();
 
+                // log in after registraion
+                LogUserIn(newMember.Email);
+
                 // Redirect to home page
                 return RedirectToAction("Index", "Home");
             }
@@ -62,7 +65,7 @@ namespace CPW219_eCommerceSite.Controllers
                 // If exist, send to home page
                 if (m != null)
                 {
-                    HttpContext.Session.SetString("Email", loginModel.Email);
+                    LogUserIn(loginModel.Email);
 
                     return RedirectToAction("Index", "Home");
                 }
@@ -72,6 +75,17 @@ namespace CPW219_eCommerceSite.Controllers
 
             // Return Page If no record found or ModelState is invalid
             return View(loginModel);
+        }
+
+        private void LogUserIn(string email)
+        {
+            HttpContext.Session.SetString("Email", email);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
 
     }
