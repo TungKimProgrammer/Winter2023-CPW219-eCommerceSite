d9e690ef8009eea671bc138f0633f168376fc741
TUNG N KIM <Tungkims@gmail.com>
2023-02-25 00:22:06 -0600

    Basic log in page functional

--- CPW219-eCommerceSite/Controllers/MembersController.cs
+++ CPW219-eCommerceSite/Controllers/MembersController.cs
@@ -2,6 +2,7 @@
 using CPW219_eCommerceSite.Models;
 using System.Security.Cryptography.Pkcs;
 using CPW219_eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace CPW219_eCommerceSite.Controllers
 {
@@ -40,5 +41,36 @@ namespace CPW219_eCommerceSite.Controllers
             }
             return View(regModel);
         }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginModel) 
+        {
+            if (ModelState.IsValid) 
+            {
+                // Check DB for credentials
+                Member? m = await (from member in _context.Members
+                                   where member.Email == loginModel.Email &&
+                                         member.Password == loginModel.Password
+                                   select member).SingleOrDefaultAsync();
+
+                // If exist, send to home page
+                if (m != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Credentials not found!");
+            }
+
+            // Return Page If no record found or ModelState is invalid
+            return View(loginModel);
+        }
+
     }
 }
