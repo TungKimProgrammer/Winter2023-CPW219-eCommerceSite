9e2ad6cdcfb0e21e40729649bc654e35b47c9c69
TUNG N KIM <Tungkims@gmail.com>
2023-02-24 22:25:50 -0600

    Added Member class

---
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Member
+    {
+        [Key] 
+        public int MemberId { get; set; }
+
+        public string Email { get; set; }   
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Username { get; set; }
+    }
+}
