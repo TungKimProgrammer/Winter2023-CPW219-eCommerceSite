435287a6f453903c1fdc6096e17d6789f524236b
TUNG N KIM <Tungkims@gmail.com>
2023-02-24 22:51:35 -0600

    Added RegisterViewmodel

--- CPW219-eCommerceSite/Models/Member.cs
+++ CPW219-eCommerceSite/Models/Member.cs
@@ -18,4 +18,26 @@ namespace CPW219_eCommerceSite.Models
 
         public string Username { get; set; }
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Email))]
+        [Display(Name ="Confirm Email")]
+        public string ConfirmedEmail { get; set; }
+
+        [Required]
+        [StringLength(75, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        [Display(Name ="Confirm Password")]
+        public string ConfirmedPassword { get; set; }
+    }
 }
